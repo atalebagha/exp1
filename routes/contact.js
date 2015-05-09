10b35243183fb11c9c8682a3c3ef5bbd19a5eb22
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-05-09 10:02:28 +0300

    mailer setup for contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact Us' });
+});
+
+router.post('/send',function(req, res, next){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user:  'test@test.com',
+      pass:  'apassword'
+    }
+  });
+
+  var mailOptions = {
+    from: 'John Dough <Johndough@test.com>',
+    to: 'test@test.com',
+    subject:  'Website Submission',
+    text: 'You have a new submission with the following details:\n\nName: ' + req.body.name + '\n\nEmail:  ' + req.body.email + '\n\nMessage:  ' + req.body.message,
+    html:  '<p>You got a new submission with the following details</p><ul><li>Name:  ' + req.body.name + '</li><li>Email:  ' + req.body.email + '</li><li>Message:  ' + req.body.message + '</li>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent:  ' + info.response);
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
